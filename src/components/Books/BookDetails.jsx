bedf86e38f960f394a93ba25cbc53deee12180b7
mushfiqueraiyan <mushfiqueraiyan1@gmail.com>
2025-04-19 14:43:12 +0600

    changes on responsive

--- src/components/Books/BookDetails.jsx
+++ src/components/Books/BookDetails.jsx
@@ -37,9 +37,9 @@ const BookDetails = () => {
       <button onClick={handleBackToHome} className="btn mt-10">
         Back
       </button>
-      <div className="flex gap-10 mt-5">
-        <div className="bg-gray-200 p-10 rounded-2xl">
-          <img src={image} alt="" width={500} />
+      <div className=" mb-10 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-2 mt-5 px-6 md:px-6 lg:px-0">
+        <div className="bg-gray-200 p-10 rounded-2xl w-120 md:w-150 justify-items-center">
+          <img src={image} alt="" className="w-[500px] h-[500px]" />
         </div>
         <div>
           <h1 className="text-5xl font-bold">{bookName}</h1>
@@ -47,7 +47,7 @@ const BookDetails = () => {
           <hr className="text-gray-300 w-150" />
           <p className="text-gray-400 my-2">{category}</p>
           <hr className="text-gray-300 w-150" />
-          <p className="text-gray-500 my-2 w-170 text-justify">
+          <p className="text-gray-500 my-2 w-120 md:w-170 lg:w-170 text-justify">
             <span className="font-bold text-black">Review </span>
             {review}
           </p>
